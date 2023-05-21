a51c5afb2ac4d4d2ca7d12f742b9a42fc98ccf14
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-21 16:16:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+window.onload = function() {
+      var sizeInfo = document.getElementById("sizeInfo");
+      var width, height;
+
+      function updateSizeInfo()
+	  {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        sizeInfo.textContent = "Width: " + width + " and Height: " + height;
+      }
+      updateSizeInfo();
+	window.addEventListener("resize", updateSizeInfo);
+    };
